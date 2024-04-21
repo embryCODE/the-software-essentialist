e62b1ae60dc48bf4d2c19ee78427d8d6df08a786
Mason Embry <mason.embry@asurion.com>
2024-04-21 10:22:43 -0500

    Refactor tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,37 +1,21 @@
 import { palindrome } from './index';
 
 describe('palindrome', () => {
-  it('should return true if the input is a "mom"', () => {
-    expect(palindrome('mom')).toBe(true);
-  });
-
-  it('should return true if the input is a "Mom"', () => {
-    expect(palindrome('Mom')).toBe(true);
-  });
-
-  it('should return true if the input is a "MoM"', () => {
-    expect(palindrome('MoM')).toBe(true);
-  });
-
-  it('should return false if the input is a "Was It A Rat I Saw"', () => {
-    expect(palindrome('Was It A Rat I Saw')).toBe(true);
-  });
-
-  it('should return false if the input is a "Never Odd or Even"', () => {
-    expect(palindrome('Never Odd or Even')).toBe(true);
-  });
-
-  it('should return false if the input is a "1Never Odd or Even1"', () => {
-    expect(palindrome('1Never Odd or Even1')).toBe(true);
-  });
-
-  // False cases
-
-  it('should return false if the input is a "Momx"', () => {
-    expect(palindrome('Momx')).toBe(false);
-  });
-
-  it('should return false if the input is a "Never Odd or Even1"', () => {
-    expect(palindrome('Never Odd or Even1')).toBe(false);
-  });
+  it.each`
+    input                    | expected
+    ${'mom'}                 | ${true}
+    ${'Mom'}                 | ${true}
+    ${'MoM'}                 | ${true}
+    ${'Momx'}                | ${false}
+    ${'xMomx'}               | ${true}
+    ${'Was It A Rat I Saw'}  | ${true}
+    ${'Never Odd or Even'}   | ${true}
+    ${'Never Odd or Even1'}  | ${false}
+    ${'1Never Odd or Even1'} | ${true}
+  `(
+    'should return $expected if the input is a "$input"',
+    ({ input, expected }) => {
+      expect(palindrome(input)).toBe(expected);
+    }
+  );
 });
