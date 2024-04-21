9ff1d26b7fa53e2b2a4a8dc3640323caa95b571e
Mason Embry <mason.embry@asurion.com>
2024-04-21 10:09:01 -0500

    Red

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,5 @@
-
-describe('palindrome checker', () => {
-
-})
+describe('palindrome', () => {
+  it('should return true if the input is a "mom"', () => {
+    expect(palindrome('mom')).toBe(true);
+  });
+});
