ef761e029bfe3ac1560422c73482ea7fb9c2299f
Mason Embry <mason.embry@asurion.com>
2024-04-21 10:10:49 -0500

    New test, still green

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -8,4 +8,8 @@ describe('palindrome', () => {
   it('should return true if the input is a "Mom"', () => {
     expect(palindrome('Mom')).toBe(true);
   });
+
+  it('should return true if the input is a "Mom"', () => {
+    expect(palindrome('MoM')).toBe(true);
+  });
 });
