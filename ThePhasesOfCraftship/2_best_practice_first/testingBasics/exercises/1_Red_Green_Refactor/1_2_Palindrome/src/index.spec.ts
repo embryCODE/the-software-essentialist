589b7324f60332fd9b4ed42b77fec2268558eb8f
Mason Embry <mason.embry@asurion.com>
2024-04-21 10:16:15 -0500

    Red, finally need to write the real impl

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -13,15 +13,25 @@ describe('palindrome', () => {
     expect(palindrome('MoM')).toBe(true);
   });
 
-  it('should return false if the input is a "Momx"', () => {
-    expect(palindrome('Momx')).toBe(false);
-  });
-
   it('should return false if the input is a "Was It A Rat I Saw"', () => {
     expect(palindrome('Was It A Rat I Saw')).toBe(true);
   });
 
   it('should return false if the input is a "Never Odd or Even"', () => {
     expect(palindrome('Never Odd or Even')).toBe(true);
+  });
+
+  it('should return false if the input is a "1Never Odd or Even1"', () => {
+    expect(palindrome('1Never Odd or Even1')).toBe(true);
+  });
+
+  // False cases
+
+  it('should return false if the input is a "Momx"', () => {
+    expect(palindrome('Momx')).toBe(false);
+  });
+
+  it('should return false if the input is a "Never Odd or Even1"', () => {
+    expect(palindrome('Never Odd or Even1')).toBe(false);
   });
 });
