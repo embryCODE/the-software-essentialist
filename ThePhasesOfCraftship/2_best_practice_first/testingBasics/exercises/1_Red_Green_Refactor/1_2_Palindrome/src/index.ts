3c83a1955ef62dd90859d2ee1ec58f0dd983498a
Mason Embry <mason.embry@asurion.com>
2024-04-21 10:07:57 -0500

    Copy in requirements

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -0,0 +1,13 @@
+// - [ ] I have committed on every single transition from red to green to refactor
+// - [ ] I have tests that validate the following statements
+//   - "mom" returns true
+//   - "Mom" returns true
+//   - "MoM" returns true
+//   - "Momx" returns false
+//   - "xMomx" returns true
+//   - "Was It A Rat I Saw" returns true
+//   - "Never Odd or Even" returns true
+//   - "Never Odd or Even1" returns false
+//   - "1Never Odd or Even1" returns true
+// - [ ] Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization (see Tip #3 here)
+// - [ ] There is no duplication in my test code or my production code
